708c385551ee6ce1ba05e9314f8f35dfff1cd653
cshim <chris.shim1999@hotmail.com>
2023-04-08 20:42:05 -0400

    Created basic UI that hits the get products endpoint and displays all products in a material ui card

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,9 @@ uvicorn main:app --reload
 from pymongo import MongoClient
 from fastapi import FastAPI
 from typing import List, Optional
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import json 
+import json
 from bson import json_util
 import customer
 import product
@@ -15,16 +16,32 @@ import supplier
 
 app = FastAPI()
 
-#Customer Endpoints
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Customer Endpoints
 @app.post("/customers")
-async def create_customer(customer_id: int, name: str, email: str, phone: str, address: str, city: str, province: str, postal_code: str, country: str):
+async def create_customer(customer_id: int, name: str, email: str, phone: str, address: str, city: str, province: str,
+                          postal_code: str, country: str):
     result = customer.insertCustomer(customer_id, name, email, phone, address, city, province, postal_code, country)
     return {"message": "Customer created successfully", "customer_id": result}
+
 
 @app.get("/customers")
 async def get_customers():
     result = customer.getCustomers()
     return {"customers": result}
+
 
 @app.get("/customers/{customer_id}")
 async def get_customer(customer_id: int):
@@ -34,26 +51,36 @@ async def get_customer(customer_id: int):
     else:
         return {"message": "Customer not found"}
 
+
 @app.put("/customers/{customer_id}")
-async def update_customer(customer_id: int, name: Optional[str] = None, email: Optional[str] = None, phone: Optional[str] = None, address: Optional[str] = None, city: Optional[str] = None, province: Optional[str] = None, postal_code: Optional[str] = None, country: Optional[str] = None):
+async def update_customer(customer_id: int, name: Optional[str] = None, email: Optional[str] = None,
+                          phone: Optional[str] = None, address: Optional[str] = None, city: Optional[str] = None,
+                          province: Optional[str] = None, postal_code: Optional[str] = None,
+                          country: Optional[str] = None):
     result = customer.updateCustomer(customer_id, name, email, phone, address, city, province, postal_code, country)
     return {"message": result}
+
 
 @app.delete("/customers/{customer_id}")
 async def delete_customer(customer_id: int):
     result = customer.deleteCustomer(customer_id)
     return {"message": result}
 
-#Product Endpoints
+
+# Product Endpoints
 @app.post("/products")
-async def create_product(product_id: int, name: str, price: float, description: str, category: str, quantity: int, supplier_id: int, supplier_name: str, supplier_email: str, supplier_phone: str):
-    result = product.insertProduct(product_id, name, price, description, category, quantity, supplier_id, supplier_name, supplier_email, supplier_phone)
+async def create_product(product_id: int, name: str, price: float, description: str, category: str, quantity: int,
+                         supplier_id: int, supplier_name: str, supplier_email: str, supplier_phone: str):
+    result = product.insertProduct(product_id, name, price, description, category, quantity, supplier_id, supplier_name,
+                                   supplier_email, supplier_phone)
     return {"message": "Product created successfully", "product_id": result}
+
 
 @app.get("/products")
 async def get_products():
     result = product.getProducts()
     return {"products": result}
+
 
 @app.get("/products/{product_id}")
 async def get_product(product_id: int):
@@ -63,26 +90,38 @@ async def get_product(product_id: int):
     else:
         return {"message": "Product not found"}
 
+
 @app.put("/products/{product_id}")
-async def update_product(product_id: int, name: Optional[str] = None, price: Optional[float] = None, description: Optional[str] = None, category: Optional[str] = None, quantity: Optional[int] = None, supplier_id: Optional[int] = None, supplier_name: Optional[str] = None, supplier_email: Optional[str] = None, supplier_phone: Optional[str] = None):
-    result = product.updateProduct(product_id, name, price, description, category, quantity, supplier_id, supplier_name, supplier_email, supplier_phone)
+async def update_product(product_id: int, name: Optional[str] = None, price: Optional[float] = None,
+                         description: Optional[str] = None, category: Optional[str] = None,
+                         quantity: Optional[int] = None, supplier_id: Optional[int] = None,
+                         supplier_name: Optional[str] = None, supplier_email: Optional[str] = None,
+                         supplier_phone: Optional[str] = None):
+    result = product.updateProduct(product_id, name, price, description, category, quantity, supplier_id, supplier_name,
+                                   supplier_email, supplier_phone)
     return {"message": result}
+
 
 @app.delete("/products/{product_id}")
 async def delete_product(product_id: int):
     result = product.deleteProduct(product_id)
     return {"message": result}
 
+
 # Order Endpoints
 @app.post("/orders")
-async def create_order(order_id: int, order_date: str, customer_id: int, customer_name: str, customer_email: str, customer_phone: str, shipping_address: dict, products: list):
-    result = order.insertOrder(order_id, order_date, customer_id, customer_name, customer_email, customer_phone, shipping_address, products)
+async def create_order(order_id: int, order_date: str, customer_id: int, customer_name: str, customer_email: str,
+                       customer_phone: str, shipping_address: dict, products: list):
+    result = order.insertOrder(order_id, order_date, customer_id, customer_name, customer_email, customer_phone,
+                               shipping_address, products)
     return {"message": "Order created successfully", "order_id": result}
+
 
 @app.get("/orders")
 async def get_orders():
     result = order.getOrders()
     return {"orders": result}
+
 
 @app.get("/orders/{order_id}")
 async def get_order(order_id: int):
@@ -92,15 +131,22 @@ async def get_order(order_id: int):
     else:
         return {"message": "Order not found"}
 
+
 @app.put("/orders/{order_id}")
-async def update_order(order_id: int, order_date: Optional[str] = None, customer_id: Optional[int] = None, customer_name: Optional[str] = None, customer_email: Optional[str] = None, customer_phone: Optional[str] = None, shipping_address: Optional[dict] = None, products: Optional[list] = None):
-    result = order.updateOrder(order_id, order_date, customer_id, customer_name, customer_email, customer_phone, shipping_address, products)
+async def update_order(order_id: int, order_date: Optional[str] = None, customer_id: Optional[int] = None,
+                       customer_name: Optional[str] = None, customer_email: Optional[str] = None,
+                       customer_phone: Optional[str] = None, shipping_address: Optional[dict] = None,
+                       products: Optional[list] = None):
+    result = order.updateOrder(order_id, order_date, customer_id, customer_name, customer_email, customer_phone,
+                               shipping_address, products)
     return {"message": result}
+
 
 @app.delete("/orders/{order_id}")
 async def delete_order(order_id: int):
     result = order.deleteOrder(order_id)
     return {"message": result}
+
 
 # Supplier Endpoints
 @app.post("/suppliers")
@@ -108,10 +154,12 @@ async def create_supplier(supplier_id: int, name: str, email: str, phone: str, p
     result = supplier.insertSupplier(supplier_id, name, email, phone, products)
     return {"message": "Supplier created successfully", "supplier_id": result}
 
+
 @app.get("/suppliers")
 async def get_suppliers():
     result = supplier.getSuppliers()
     return {"suppliers": result}
+
 
 @app.get("/suppliers/{supplier_id}")
 async def get_supplier(supplier_id: int):
@@ -121,15 +169,19 @@ async def get_supplier(supplier_id: int):
     else:
         return {"message": "Supplier not found"}
 
+
 @app.put("/suppliers/{supplier_id}")
-async def update_supplier(supplier_id: int, name: Optional[str] = None, email: Optional[str] = None, phone: Optional[str] = None, products: Optional[List[dict]] = None):
+async def update_supplier(supplier_id: int, name: Optional[str] = None, email: Optional[str] = None,
+                          phone: Optional[str] = None, products: Optional[List[dict]] = None):
     result = supplier.updateSupplier(supplier_id, name, email, phone, products)
     return {"message": result}
+
 
 @app.delete("/suppliers/{supplier_id}")
 async def delete_supplier(supplier_id: int):
     result = supplier.deleteSupplier(supplier_id)
     return {"message": result}
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "./components/productCard";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+  fetch("http://localhost:8000/products", {
+    method:"GET"
+  }).then(response => response.json())
+  .then(data => {
+    console.log(data);
+    setData(data);
+  })
+  .catch((err) => {
+         console.log(err.message);
+  });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="productCardContainer">
+    {data ? (
+      data.products.map((product) => (
+        <ProductCard product={product} />
+      ))
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
   );
 }
 
 export default App;
+
